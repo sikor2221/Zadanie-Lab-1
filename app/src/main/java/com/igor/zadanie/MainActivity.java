0e9f42146704b5eebd07fcbaff4993ed608f8529
igor <plajta12@gmail.com>
2019-03-29 20:47:04 +0100

    eloelo

--- app/src/main/java/com/igor/zadanie/MainActivity.java
+++ app/src/main/java/com/igor/zadanie/MainActivity.java
@@ -2,12 +2,33 @@ package com.igor.zadanie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+    Random mRandomNumber;
+    Integer mNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.numberTV);
+        mRandomNumber = new Random();
+        mNumber = mRandomNumber.nextInt(100);
+
+        mTextView.setText(mNumber.toString());
+        if(mNumber >= 40){
+            Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
+        } else if (mNumber <40){
+            Toast.makeText(this, "...", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
